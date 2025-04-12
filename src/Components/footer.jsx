a74791671655d50ba844caaa06a4f15ec32147b8
Syed Muhammad Mehran <smehran.mme@gmail.com>
2025-04-12 16:57:45 +0500

    Event Planner App

---
+++ src/Components/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const footer_event = () => {
+    return (
+        <>
+        <footer>
+            <p>&copy; Event Planner Organization. All Rights Reserved.</p>
+        </footer>
+        <div>This is a division inside footer</div>
+        </>
+    )
+}
+
+export default footer_event;
